cd415f84dbe175ddfff6668ad8dc085598a9e4f7
Young Ahn <young_ahn@yonsei.ac.kr>
2019-09-16 00:04:59 +0900

    stop crawling until empty dataframe

--- crawling.py
+++ crawling.py
@@ -4,14 +4,23 @@ import pandas as pd
 import requests
 from bs4 import BeautifulSoup
 
-res = requests.get("https://oia.yonsei.ac.kr/partner/expReport.asp?ucode=DK000003&bgbn=A")
-soup = BeautifulSoup(res.content,'lxml')
-table = soup.find_all('table')[0]
-# df = pd.read_html(str(table),encoding='utf-8')
-# print(table)
-df = pd.read_html(str(table),encoding='utf-8', header=0)[0]
-df['href'] = [np.where(tag.has_attr('href'),tag.get('href'),"no link") for tag in table.find_all('a')]
-# print(df[0].to_json(orient='records')) #this one turns table texts into json
-# print(df) #this one prints mere table texts
-print(df['href'])
+page = 1
+while page < 10:
+    univ_query = "DK000003"
+    url = "https://oia.yonsei.ac.kr/partner/expReport.asp?page=" + str(page)+"&cur_pack=0&ucode="+str(univ_query)+"&bgbn=A"
+    res = requests.get(url)
+    soup = BeautifulSoup(res.content,'lxml')
+    table = soup.find_all('table')[0]
+    # df = pd.read_html(str(table),encoding='utf-8')
+    # print(table)
+    df = pd.read_html(str(table),encoding='utf-8', header=0)[0]
+    df['href'] = [np.where(tag.has_attr('href'),tag.get('href'),"no link") for tag in table.find_all('a')]
+    # print(df[0].to_json(orient='records')) #this one turns table texts into json
+    # print(df) #this one prints mere table texts
+    print(df['href'])
+    # print(df.index)
+    if not df.empty:
+        page += 1
+    else:
+        break
 
