fd61ff3789a104a9b19b7ff305f4ba36858aa019
Young Ahn <young_ahn@yonsei.ac.kr>
2019-09-15 22:35:59 +0900

    obtained url from table text

--- crawling.py
+++ crawling.py
@@ -1,23 +1,5 @@
-from selenium import webdriver
-import time
-
-options = webdriver.ChromeOptions()
-driver = webdriver.Chrome(r"C:\Users\pc\Desktop\chromedriver")
-
-url = 'https://oia.yonsei.ac.kr/partner/expReport.asp?ucode=DK000003&bgbn=A'
-
-driver.get(url)
-
-time.sleep(3)
-
-e = driver.find_element_by_class_name('')
-print(e.text)
-
-driver.close()
-
-
-"""
-# This one gets all table data text 
+# This one gets all table data text
+import numpy as np
 import pandas as pd
 import requests
 from bs4 import BeautifulSoup
@@ -25,7 +7,11 @@ from bs4 import BeautifulSoup
 res = requests.get("https://oia.yonsei.ac.kr/partner/expReport.asp?ucode=DK000003&bgbn=A")
 soup = BeautifulSoup(res.content,'lxml')
 table = soup.find_all('table')[0]
-df = pd.read_html(str(table),encoding='utf-8')
-# print(df[0].to_json(orient='records'))
-print(df)
-"""
+# df = pd.read_html(str(table),encoding='utf-8')
+# print(table)
+df = pd.read_html(str(table),encoding='utf-8', header=0)[0]
+df['href'] = [np.where(tag.has_attr('href'),tag.get('href'),"no link") for tag in table.find_all('a')]
+# print(df[0].to_json(orient='records')) #this one turns table texts into json
+# print(df) #this one prints mere table texts
+print(df['href'])
+
