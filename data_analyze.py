8240bd5100a817e97db053c591e5020c0aba2708
Young Ahn <young_ahn@yonsei.ac.kr>
2019-11-03 19:15:14 +0900

    soynlp tokenization

---
+++ data_analyze.py
@@ -0,0 +1,25 @@
+#-*- coding:utf-8 -*-
+
+import pandas as pd
+from soynlp.tokenizer import RegexTokenizer
+tokenizer = RegexTokenizer()
+import numpy as np
+import re
+
+
+df = pd.read_csv(r'C:/Users/pc/Documents/GitHub/OIA_Text_Wrangling/DK000003.csv_text_data.csv')
+
+print(df.shape)
+
+# print(df.tail)
+
+basic_info_df = df['gen_info']
+# print(basic_info_df)
+
+
+sample_index = 24
+sample_content =basic_info_df[sample_index]
+
+# print(sample_content)
+tokenized_content = tokenizer.tokenize(sample_content)
+print(tokenized_content)
